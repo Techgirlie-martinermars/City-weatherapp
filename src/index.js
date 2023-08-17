f6f44318578b4496a346e5e8c06f0dbcdcf74d91
Tecggirlie-martinermars <131309536+Techgirlie-martinermars@users.noreply.github.com>
2023-08-17 20:52:11 +0300

    modifying forecastHTML
    added the maximum and minimum temp into the injected forecast html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,84 @@
+function formatDate(timestamp) {
+  console.log("timestamp");
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  console.log("timestamp");
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  console.log("response");
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy${
+            forecastDay.description.icon
+          }.png"
+          alt=""
+          width="30"
+        />
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temperature.max
+          )}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temperature.min
+          )}°</span>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function displayTemperature(response) {
+  console.log("response");
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
@@ -13,95 +93,48 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
 
-  let dateElement = document.querySelector("#currentDate");
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let now = new Date();
-  let day = days[now.getDay()];
-  let hours = now.getHours().toString().padStart(2, "0");
-  let minutes = now.getMinutes().toString().padStart(2, "0");
-
-  let time = `${hours}:${minutes}`;
-  currentDate.innerHTML = `${day},${time}`;
-
-  function displayForecast() {
-    let forecastElement = document.querySelector("#forecast");
-
-    let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
-
-    let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-          alt=""
-          width="36"
-        />
-        <div class="weather-forecast-temperature">
-          <span class="weather-forecast-temperature-max">24°</span>
-          <span class="weather-forecast-temperature-min">18°</span>
-        </div>
-      </div>
-    `;
-    });
-
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
-  }
-
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/04d@2x.png`
   );
+  getForecast(response.data.coord);
 }
-document.addEventListener("DOMContentLoaded", function () {
-  function search(city) {
-    let apiKey = "e37c6b88980f0619762528f7461327ae";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
-  }
 
-  function showFahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    let FahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(FahrenheitTemperature);
+function search(city) {
+  console.log("city");
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    displayForecast();
-  }
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  console.log("event");
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-  function showCelsiusTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  }
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let FahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(FahrenheitTemperature);
+}
 
-  let form = document.querySelector("#search-form");
-  form.addEventListener("submit", handleSubmit);
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", showCelsiusTemperature);
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
-  search("Nairobi");
-});
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+search("Nairobi");
